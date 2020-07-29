9bb041c1737697fead03abf0e0d8179d80d404b5
Mihaly Lestak <ws.eclipse@gmail.com>
2020-07-29 16:06:24 +0200

    farmanimals done

---
+++ week03/day01/src/Animals/Farm.java
@@ -0,0 +1,46 @@
+package Animals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Farm {
+  List<Animals> animalsOnFarm = new ArrayList<>();
+  int slots;
+
+  public Farm() {
+    this.slots = 10;
+  }
+
+  public Farm(List<Animals> animalsOnFarm) {
+    this.animalsOnFarm = animalsOnFarm;
+  }
+
+  public int getNumberOfAnimals() {
+    return animalsOnFarm.size();
+  }
+
+  public Animals getAnimal(int index) {
+    return animalsOnFarm.get(index);
+  }
+
+  public void breed() {
+    if (animalsOnFarm.size() < slots) {
+      animalsOnFarm.add(new Animals());
+    } else {
+      System.out.println("Too many animals!");
+    }
+  }
+
+  public void slaughter() {
+    int starterHungry = animalsOnFarm.get(0).getHunger();
+    Animals leastHungry = animalsOnFarm.get(0);
+    for (int i = 0; i < animalsOnFarm.size(); i++) {
+      if (animalsOnFarm.get(i).getHunger() < starterHungry) {
+        starterHungry = animalsOnFarm.get(i).getHunger();
+        leastHungry = animalsOnFarm.get(i);
+      }
+    }
+  animalsOnFarm.remove(leastHungry);
+
+  }
+}
