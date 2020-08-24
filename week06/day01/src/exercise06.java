5fc839fe906b1890b6ced8f45d80ad393fa1249e
Mihaly Lestak <ws.eclipse@gmail.com>
2020-08-24 17:34:34 +0200

    day01 exercise6 added

--- week06/day01/src/exercise06.java
+++ week06/day01/src/exercise06.java
@@ -3,14 +3,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class exercise06 {
+
+
   public static void main(String[] args) {
-
     String theText = "a GreenFox Once Fell Into A Fox Trap";
-//    List<Character> theWorkableText = new ArrayList<Character>(theText.toCharArray());
-
-//    System.out.println(theWorkableText);
-
+    theText.chars()
+        .filter(x -> x == ((int)(Character.toString((char)x).toUpperCase()).charAt(0)))
+        .forEach(x -> System.out.println((char)x));
   }
 }
 
 //    Write a Stream Expression to find the uppercase characters in a string!
+
